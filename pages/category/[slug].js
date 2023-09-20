c5581b4fd31e674fdbe83722f06c90bcbaab26c5
Mazzaroth.dev <mazzaroth.dev@gmail.com>
2023-09-20 16:43:27 -0300

    test modify3

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -90,7 +90,7 @@ const Category = ({ category, products, slug }) => {
 export default Category;
 
 export async function getStaticPaths() {
-    const category = await fetchDataFromApi("/api/categories?populate=*");    
+    const category = await fetchDataFromApi("/api/categories?populate=*");
     const paths = category?.data?.map((c) => ({
         params: {
             slug: c.attributes.slug,
